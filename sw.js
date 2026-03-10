4812cc111d60e1076ea09f90eb574657e4d22ab4
Nelsonpi84 <sanagustingijon@gmail.com>
2026-03-10 13:38:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,2 @@
-const CACHE='gc-rebuilt-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
